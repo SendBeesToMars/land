7a1f31f952055397758430c9d3300daccdb1439c
SendBeesToMars <18416329+SendBeesToMars@users.noreply.github.com>
2024-07-17 00:33:52 +0100

    eat sleep rave repeat

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
 
 
-let interval_timers = [];
-let current_selection = null;
+let stuff = {
+    interval_timers: [],
+    current_selection: null,
+}
 
-function initialise_ui() {
+{
     let inventory = document.getElementsByClassName("inv");
     for (var i = 0; i < inventory.length; i++) {
-        inventory[i].innerHTML = localStorage.getItem(inventory[i].id.substring(0, inventory[i].id.length - 4));
+        inventory[i].innerHTML = localStorage.getItem(
+            inventory[i].id.substring(0, inventory[i].id.length - "_num".length));
     }
 }
-initialise_ui();
 
-function gather(id) {
+function gather(id, increment = 2) {
+    clear_EVERYTHING(id);
+    stuff.interval_timers.push(setInterval(item_update, 500, id, increment));
+}
+
+function consoom(id, resource_id, decrement = -10) {
+    clear_EVERYTHING(id)
+    stuff.interval_timers.push(setInterval(item_update, 500, resource_id, decrement));
+}
+
+function clear_EVERYTHING(id) {
     clear_timers();
-    if (current_selection  === id){
-        current_selection = null;
+    if (stuff.current_selection  === id){
+        stuff.current_selection = null;
         document.getElementById(id).style.borderStyle = 'outset';
         return;
     }
-    current_selection = id;
+    stuff.current_selection = id;
     let gather = document.getElementsByClassName("gather");
     for (var i = 0; i < gather.length; i++) {
         gather[i].style.borderStyle = 'outset';
     }
     document.getElementById(id).style.borderStyle = 'inset';
-    
-    const intervalID = setInterval(increment_item, 500, id, 1);
-    interval_timers.push(intervalID);
 }
 
 function update_ui(id) {
     document.getElementById(id + "_num").textContent = localStorage.getItem(id);
 }
 
-function increment_item(name, increment) {
+function item_update(name, change) {
     const item = localStorage.getItem(name);
-    if (item < 1) {
-        localStorage.setItem(name, 1);
-    }else{
-        localStorage.setItem(name, parseInt(item) + increment);
-    }
-    console.log(localStorage.getItem(name));
+    !item ? localStorage.setItem(name, change) :
+        localStorage.setItem(name, parseInt(item) + change);
     update_ui(name);
 }
 
 function clear_timers() {
-    for (var i = 0; i < interval_timers.length; i++) {
-        clearInterval(interval_timers[i]);
+    for (var i = 0; i < stuff.interval_timers.length; i++) {
+        clearInterval(stuff.interval_timers[i]);
     }
 }
